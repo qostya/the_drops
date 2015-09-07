faba08f6f88c46bec093cce5d4a967212f8fd34f
Constantine <qostyab@gmail.com>
2015-09-07 07:37:48 +0300

    added b-index-content_* (_services)

--- js/main.js
+++ js/main.js
@@ -32,7 +32,9 @@ $(function ($) {
         });
 
         $('.b-header_search_icon').click(function () {
-            if (!/__active/.test($(this).siblings('.b-header_search_field').attr('class'))) {
+            var $searchField = $('.b-header_search_field');
+            if (!/__active/.test($searchField.attr('class')) ||
+                !$searchField.find('input').val()) {
                 return false;
             }
         });
@@ -43,6 +45,23 @@ $(function ($) {
         pager: false
     });
 
+
+    (function () {
+        var sett;
+        $('.js-has-dropdown').hover(function () {
+            var $this = $(this);
+            clearTimeout(sett);
+
+            $this.addClass('js-has-dropdown__active');
+            $(this).find('.js-dropdown').slideDown('fast');
+        }, function () {
+            var $this = $(this);
+            sett = setTimeout(function () {
+                $this.removeClass('js-has-dropdown__active');
+                $this.find('.js-dropdown').slideUp('fast');
+            }, 1000);
+        });
+    }());
 
 
     showByHoverSiblings('js-show-target', 'js-show-element');
